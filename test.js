20d0f9ca3f9c87345415fa71e3c4f727a50725a3
Jordi Galobart Salvat <jgalobart@gmail.com>
2024-10-14 20:28:29 +0200

    add npm

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const nombre = "Jordi";
 
-console.log(nombre);
+console.log("hola, me llamo, " + nombre);
 
 function restaAdditiva(a, b) {
   return a + b;
@@ -8,4 +8,12 @@ function restaAdditiva(a, b) {
 
 function suma(a, b) {
   return a + b;
+}
+
+function addTask() {
+  const task = document.getElementById("task").value;
+  const taskList = document.getElementById("taskList");
+  const newTask = document.createElement("li");
+  newTask.innerText = task;
+  taskList.appendChild(newTask);
 }
